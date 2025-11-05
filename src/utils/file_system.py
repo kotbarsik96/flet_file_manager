667625f79302279ad33e2fc85a860a261151ab6d
Алексей <kotbarsik96@gmail.com>
2025-11-05 22:27:55 +0400

    Получаем размер по-другому

--- src/utils/file_system.py
+++ src/utils/file_system.py
@@ -6,30 +6,21 @@ def format_bytes_to_string(file_size_bytes: int, round_to_decimals: int = 0) -> 
 
     for unit in ("Байт", "КБ", "МБ", "ГБ", "ТБ"):
         if abs(num) < 1024.0:
-            if round_to_decimals > 0: 
+            if round_to_decimals > 0:
                 num = round(num, round_to_decimals)
-            else: 
+            else:
                 num = round(num)
             return f"{num} {unit}"
         num = num / 1024.0
 
 
 def get_dir_size(directoryPath: str) -> int:
-    total = 0
+    size = 0
 
     try:
-        with os.scandir(directoryPath) as it:
-            for entry in it:
-                try:
-                    if entry.is_dir():
-                        total += get_dir_size(entry.path)
-                    else:
-                        total += entry.stat().st_size
-                except (PermissionError, FileNotFoundError):
-                    continue
-    except (PermissionError, FileNotFoundError):
-        return 0
+        for el in os.scandir(directoryPath):
+            size += os.path.getsize(el)
+    except (FileNotFoundError, PermissionError):
+        pass
 
-    return total
-
-    
+    return size
